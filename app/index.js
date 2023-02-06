5b4bb27085ab2a9dad5acdb567279519853131e0
amirhnajafiz <najafizadeh21@gmail.com>
2023-02-06 11:56:23 +0330

    feat: redis connection

--- app/index.js
+++ app/index.js
@@ -1,6 +1,18 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const redis = require("redis");
+
+const app = express();
 const port = process.env.HTTP_PORT || 3000;
+
+let redisClient;
+
+(async () => {
+    redisClient = redis.createClient();
+
+    redisClient.on("error", (error) => console.error(`Error : ${error}`));
+
+    await redisClient.connect();
+})();
 
 app.get('/api', (req, res) => {
     res.send('Hello World!')
