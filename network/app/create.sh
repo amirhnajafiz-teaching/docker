795a2bbf34fb2876caf4406100dfe35eb4278519
amirhnajafiz <najafizadeh21@gmail.com>
2023-02-06 11:45:17 +0330

    add: directory structure

---
+++ network/app/create.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+# with this script we create a network for our application and redis database.
---
+++ volume/redis/create.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+# in this script we are building a volume for our redis container.
