4b541ac74413dc9c4bef4e5b784cd92b31fd0346
amirhnajafiz <najafizadeh21@gmail.com>
2023-02-07 11:52:14 +0330

    add: docker container exec

--- run/app/exec.sh
+++ run/app/exec.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 # in this script we start our application container
-docker
+docker run --network=node-app --name node-app-container -d -p 8080:8080 amirhossein21/node-app:v0.1.0
