1fbc313b8a6911bada22b06cb47ac48196a6a55a
amirhnajafiz <najafizadeh21@gmail.com>
2023-02-07 11:59:57 +0330

    add: nginx build and run script

--- run/nginx/exec.sh
+++ run/nginx/exec.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env bash
 
 # in this script we start our nginx container
+docker build . -f build/nginx/Dockerfile -t amirhossein21/nginx:v0.1.0
+docker run --build-arg HTML_DIR=/build/nginx/html -p 80:80 -d amirhossein21/nginx:v0.1.0
