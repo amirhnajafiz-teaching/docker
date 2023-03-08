6109b9a2e4ebea6b4904e41b9d5c7f2d1190d1ad
amirhnajafiz <najafizadeh21@gmail.com>
2023-03-08 13:07:06 +0330

    fix: nginx errors

--- run/nginx/exec.sh
+++ run/nginx/exec.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
 # in this script we start our nginx container
-docker build . -f build/nginx/Dockerfile -t amirhossein21/nginx:v0.1.0
-docker run --build-arg HTML_DIR=/build/nginx/html -p 80:80 -d amirhossein21/nginx:v0.1.0
+# docker build . -f build/nginx/Dockerfile -t amirhossein21/nginx:v0.1.0
+docker build . -f build/nginx/Dockerfile --build-arg HTML_DIR=/build/nginx/html -t amirhossein21/nginx:v0.1.0
+docker run -p 80:80 -d amirhossein21/nginx:v0.1.0
