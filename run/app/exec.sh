5b204dab0efb9610a2a0b0850405843848d5d8f3
amirhnajafiz <najafizadeh21@gmail.com>
2023-03-08 13:58:09 +0330

    add: run scripts for app

--- run/app/exec.sh
+++ run/app/exec.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 
 # in this script we start our application container
+docker build . -f build/app/Dockerfile -t amirhossein21/node-app:v0.1.0
 docker run --network=node-app --name node-app-container -d -p 8080:8080 amirhossein21/node-app:v0.1.0
